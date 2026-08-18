47599ae3ca69079f3d4c67a88c7c7d7d84527d91
Franco Ortega <francopdx@gmail.com>
2026-08-18 12:52:16 -0700

    create initial manual form and response

--- src/app/manual/page.tsx
+++ src/app/manual/page.tsx
@@ -1,4 +1,44 @@
-function page() {
-	return <div>page</div>;
+'use client';
+
+import { useState } from 'react';
+import { getRandomWord } from '@/lib/wordle';
+
+function Manual() {
+	const [guess, setGuess] = useState('');
+	const targetWord = getRandomWord();
+
+	return (
+		<div>
+			<header>
+				<h1>Wordle Manual</h1>
+			</header>
+
+			<main>
+				<p>This is the Wordle manual.</p>
+
+				<section>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							if (guess === targetWord) {
+								alert('You guessed the word!');
+							}
+						}}
+					>
+						<label htmlFor='guess'>Guess:</label>
+						<input
+							type='text'
+							id='guess'
+							name='guess'
+							onChange={(e) => setGuess(e.target.value)}
+						/>
+						<button type='submit'>Submit</button>
+					</form>
+				</section>
+
+				<section>Answer: {guess}</section>
+			</main>
+		</div>
+	);
 }
-export default page;
+export default Manual;
